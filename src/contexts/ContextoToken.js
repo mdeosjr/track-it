d1a1cdd1b33bb9c136c2d10d40f9c57b9faccfc5
leo <celsolnv27@gmail.com>
2022-01-12 10:24:56 -0300

    Preparando revisão de código

--- src/contexts/ContextoToken.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const ContextoToken = createContext();
-
-export default ContextoToken;
--- src/contexts/ContextoUsuario.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const ContextoUsuario = createContext();
-
-export default ContextoUsuario;
